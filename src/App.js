65a031404b7931d1487d15b37af2af6244b97d5e
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2023-03-27 23:45:43 +0530

    ft. camera functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef, useState } from "react";
+import "./App.css";
+import Webcam from "react-webcam";
 
 function App() {
+  const [img, setImg] = useState(null);
+  const [imageFile, setimageFile] = useState(null);
+
+  const webcamRef = useRef(null);
+
+  const videoConstraints = {
+    width: { min: 480 },
+    height: { min: 720 },
+    facingMode: { exact: "user" },
+  };
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImg(imageSrc);
+    // setimageFile(img.file)
+    console.log(img);
+  }, [webcamRef]);
+
+  // Handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Convert Base64 to file
+    const blob = base64ToBlob(img);
+    const file = new File([blob], "AISight_capture.png", { type: "image/png" });
+
+    console.log(file)
+    // Send file as form data to backend
+    const formData = new FormData();
+    formData.append("image", file);
+
+    // Make a POST request to backend API
+    // fetch("http://example.com/upload", {
+    //   method: "POST",
+    //   body: formData,
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data))
+    //   .catch((error) => console.log(error));
+  };
+
+  // Convert Base64 to blob
+  const base64ToBlob = (base64) => {
+    const byteString = atob(base64.split(",")[1]);
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: "image/png" });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-purple-600 w-screen h-screen flex flex-col items-center justify-center">
+      {/* <Webcam imageSmoothing={true} width={480} height={720} videoConstraints={videoConstraints}/> */}
+
+      {img === null ? (
+        <>
+          <Webcam
+            audio={false}
+            mirrored={true}
+            height={800}
+            width={400}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+          <button onClick={capture} className="bg-white rounded-md p-4">
+            Capture photo
+          </button>
+        </>
+      ) : (
+        <>
+          <img src={img} alt="screenshot" />
+          <form onSubmit={handleSubmit}>
+            <button type="submit" className="bg-green-300 rounded-md p-4">Submit</button>
+          </form>
+
+          <button
+            onClick={() => setImg(null)}
+            className="bg-white rounded-md p-4"
+          >
+            Retake
+          </button>
+        </>
+      )}
     </div>
   );
 }
